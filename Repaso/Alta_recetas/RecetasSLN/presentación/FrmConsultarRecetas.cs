45cff171e9ed5c770e8cc88619485f2b9180cb48
FrancoL42 <111024700+FrancoL42@users.noreply.github.com>
2022-09-27 23:44:01 -0300

    Add files via upload

---
+++ Repaso/Alta_recetas/RecetasSLN/presentación/FrmConsultarRecetas.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RecetasSLN.presentación;
+
+namespace RecetasSLN.presentación
+{
+    public partial class FrmConsultarRecetas : Form
+    {
+        public FrmConsultarRecetas()
+        {
+            InitializeComponent();
+          
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            FrmNuevaReceta nueva = new FrmNuevaReceta();
+            nueva.ShowDialog();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Desea salir?", "CONFIRMACION", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                this.Close();
+        }
+        private bool validar()
+        {
+            if(cboTipoReceta.SelectedIndex == - 1)
+            {
+                return false;
+            }
+            if(txtNombre.Text == "")
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
